e664e0cb6f0ef1bcb7f1fb17638b1071a8d5945c
012e <huyphmnhat@gmail.com>
2024-02-14 00:07:29 +0700

    fix: transparent windows don't overlap on each other (in monocle mode)

--- dwm.c
+++ dwm.c
@@ -999,6 +999,8 @@ void focus(Client *c) {
     XDeleteProperty(dpy, root, netatom[NetActiveWindow]);
   }
   selmon->sel = c;
+  if (selmon->lt[selmon->sellt]->arrange == monocle)
+    arrangemon(selmon);
   drawbars();
 }
 
@@ -1319,8 +1321,15 @@ void monocle(Monitor *m) {
   for (c = m->clients; c; c = c->next)
     if (ISVISIBLE(c))
       n++;
-  for (c = nexttiled(m->clients); c; c = nexttiled(c->next))
+  for (c = m->stack; c && (!ISVISIBLE(c) || c->isfloating); c = c->snext);
+  if (c && !c->isfloating) {
+    XMoveWindow(dpy, c->win, m->wx, m->wy);
     resize(c, m->wx, m->wy, m->ww - 2 * c->bw, m->wh - 2 * c->bw, 0);
+    c = c->snext;
+  }
+  for (; c; c = c->snext)
+    if (!c->isfloating && ISVISIBLE(c))
+      XMoveWindow(dpy, c->win, WIDTH(c) * -2, c->y);
 }
 
 void motionnotify(XEvent *e) {
